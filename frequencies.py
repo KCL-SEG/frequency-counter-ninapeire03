711cf1b4bdc9d1818dda4e33de9635c08fa1de16
Nina Peire <nina.1.peire@kcl.ac.uk>
2022-09-25 17:19:36 +0100

    Complete Frequency Counter Assignment

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,12 @@
 """Frequencies function."""
-"""ENTER YOUR SOLUTION HERE!"""
+"""Function which produces a dictionary based on the frequency of items in the items list."""
 
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
-    return frequencies
+    try:
+        items = [str(i) for i in items]
+        frequencies = {}
+        for i in items:
+            frequencies = {i : items.count(i) for i in items}
+        return frequencies
+    except ValueError:
+        "You did not enter a list correctly."
